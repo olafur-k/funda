7b548d421d7a945c06ba683bd3673c99fcf42acc
olaf <olafkunowski@gmail.com>
2020-06-03 12:06:13 +0200

    Added basic logic for fetching API results

--- Funda.ApiClient/Base/FundaApiClient.cs
+++ Funda.ApiClient/Base/FundaApiClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Funda.Dto.Base;
@@ -9,9 +11,27 @@ namespace Funda.ApiClient.Base
 {
     public class FundaApiClient<T>
     {
-        public PagedResult<T> GetResults()
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<PagedResult<T>> GetAllResultsAsync(string searchTerm)
         {
-            return null;
+            var baseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"];
+            var pageSize = ConfigurationManager.AppSettings["ApiPageSize"];
+            var key = ConfigurationManager.AppSettings["ApiKey"];
+
+            var urlWithoutPage = baseUrl
+                .Replace("{KEY}", key)
+                .Replace("{PAGESIZE}", pageSize)
+                .Replace("{SEARCHTERM}", searchTerm);
+
+            var page = 1;
+
+            var urlWithPage = urlWithoutPage.Replace("{PAGE}", page.ToString());
+
+            var firstPageResponse = await _httpClient.GetAsync(urlWithPage);
+            var firstPageResults = await firstPageResponse.Content.ReadAsAsync<PagedResult<T>>();
+
+            return firstPageResults;
         }
     }
 }
--- Funda.ApiClient/HuisApiClient.cs
+++ Funda.ApiClient/HuisApiClient.cs
@@ -5,10 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using Funda.ApiClient.Base;
 using Funda.Dto;
+using Funda.Dto.Base;
 
 namespace Funda.ApiClient
 {
     public class HuisApiClient : FundaApiClient<Huis>
     {
+        public async Task<PagedResult<Huis>> GetAllAmsterdamObjectsAsync()
+        {
+            return await base.GetAllResultsAsync("/amsterdam/");
+        }
+
+        public async Task<PagedResult<Huis>> GetAllAmsterdamGardenObjectsAsync()
+        {
+            return await base.GetAllResultsAsync("/amsterdam/tuin/");
+        }
     }
 }
